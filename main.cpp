4a40333f22b54be214eeef41d2d270c868e92cd4
tonklep <anthony.klepacki@gmail.com>
2019-12-02 18:37:09 +0000

    Added hitable abstract class, sphere class, and list of hitable object to track hits.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 //Main File
 
 #include <iostream>
+#include "sphere.h"
+#include "hitable_list.h"
+#include <float.h>
 #include "vec3.h"
 #include "ray.h"
 
@@ -12,39 +15,27 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
-float hit_sphere(const vec3& center, float radius, const ray&r){
-    vec3 oc = r.origin() - center;
-    
-    float a = dot(r.direction(),r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc)-radius*radius;
-    float discriminant = b*b - 4*a*c;
-    
-    if (discriminant < 0){
-        return -1.0;
-    }else{
-        return (-b - sqrt(discriminant))/(2.0*a);
-    }
-}
 
-vec3 color(const ray& r){
-    float t = hit_sphere(vec3(0,0,-1),0.5, r);
-    
-    if (t>0.0){
-        vec3 normal = unit_vector(r.point_at_parameter(t)-vec3(0,0,-1));
-        return 0.5*vec3(normal.x()+1, normal.y()+1, normal.z()+1);
+vec3 color(const ray& r, hitable *world){
+    hit_record rec;
+    if (world->hit(r, 0.0, 1000000 , rec)){
+        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+        
+    }else{
+        //std::cout << "background" << std::endl;
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
     }
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0);
+    
 }
 
 int main(){
     int x, y, n;
 
     //Pixel initializations
-    int nx = 200;
-    int ny = 100;
+    int nx = 800;
+    int ny = 600;
     
     unsigned char data[nx * ny * 3];
     int index = 0;
@@ -54,6 +45,11 @@ int main(){
     vec3 vertical(0.0,2.0,0.0);
     vec3 origin(0.0,0.0,0.0);
     
+    hitable *list[2];
+    list[0] = new sphere(vec3(0,0,-1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable *world = new hitable_list(list, 2);
+    
     // for loop for outputting image information to file
     for (int j = ny-1; j>= 0; j--){
         for (int i = 0; i < nx; i++) {
@@ -62,7 +58,9 @@ int main(){
             float v = float(j)/float(ny);
 
             ray r(origin, lower_left_corner + (u*horizontal) + (v*vertical));
-            vec3 colorAtRay = color(r);
+            
+            vec3 p = r.point_at_parameter(2.0);
+            vec3 colorAtRay = color(r, world);
             
             int ir = int(255.99*colorAtRay[0]);
             int ig = int(255.99*colorAtRay[1]);
@@ -71,7 +69,10 @@ int main(){
             data[index++] = ir;
             data[index++] = ig;
             data[index++] = ib;
+            
+            
         }
+        
     }
     // write data to jpg image using stb library
     stbi_write_jpg("foo_out.jpg",nx, ny,3,data,100);
