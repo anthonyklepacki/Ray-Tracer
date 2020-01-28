18b66e6d6b3f0d2a596b4b669dfb6412ea03d05b
tonklep <anthony.klepacki@gmail.com>
2020-01-28 19:56:21 +0000

    added solid textures

--- main.cpp
+++ main.cpp
@@ -46,10 +46,12 @@ vec3 color(const ray& r, hitable *world, int depth){
 }
 
 hitable *random_scene() {
-    int n = 50000;
+    int n = 500;
     hitable **list = new hitable*[n+1];
-    list[0] =  new sphere(vec3(0,-1000,0), 1000,new lambertian(vec3(random_double()*random_double(),random_double()*random_double(),random_double()*random_double())));
+    texture *checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)),new constant_texture(vec3(0.9, 0.9, 0.9)));
+    list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(checker));
     int i = 1;
+    /*
     for (int a = -10; a < 10; a++) {
         for (int b = -10; b < 10; b++) {
             float choose_mat = random_double();
@@ -75,9 +77,11 @@ hitable *random_scene() {
             }
         }
     }
+    */
+    
 
     list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(new constant_texture(vec3(0.5, 0.5, 0.5))));
     list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
 
     return new hitable_list(list,i);
