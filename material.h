1e6fbcb266bd8aef6030b5bbcc15967958111283
tonklep <anthony.klepacki@gmail.com>
2020-01-30 18:11:19 +0000

    added volume objects (e.g. smoke)

--- material.h
+++ material.h
@@ -124,6 +124,22 @@ class dielectric : public material {
         float ref_idx;
 };
 
+class isotropic : public material {
+    public:
+        isotropic(texture *a) : albedo(a) {}
+        virtual bool scatter(
+            const ray& r_in,
+            const hit_record& rec,
+            vec3& attenuation,
+            ray& scattered) const {
+
+            scattered = ray(rec.p, random_in_unit_sphere());
+            attenuation = albedo->value(rec.u, rec.v, rec.p);
+            return true;
+        }
+        texture *albedo;
+};
+
 class diffuse_light : public material {
     public:
         diffuse_light(texture *a) : emit(a) {}
